c091707897dd4ab0cc0a5b55ae13c4198695d592
ghj-gg <932747552@qq.com>
2020-08-18 23:53:25 +0800

    "2020.8.18 文件上传、下载；监控端点"

--- java_spring_boot/src/main/java/com/sfac/javaSpringBoot/modules/test/controller/TestController.java
+++ java_spring_boot/src/main/java/com/sfac/javaSpringBoot/modules/test/controller/TestController.java
@@ -5,15 +5,27 @@ import com.sfac.javaSpringBoot.modules.test.entity.Country;
 import com.sfac.javaSpringBoot.modules.test.service.CityService;
 import com.sfac.javaSpringBoot.modules.test.service.CountryService;
 import com.sfac.javaSpringBoot.modules.test.vo.ApplicationTest;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import sun.nio.ch.IOUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -116,6 +128,152 @@ public class TestController {
     }
 
     /**
+     * 127.0.0.1/test/file ----- post
+     */
+    @PostMapping(value = "/file",consumes = "multipart/form-data")
+    public String uploadFiles(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes){
+        if (file.isEmpty()){
+            redirectAttributes.addFlashAttribute("message","Please select file.");
+            return "redirect:/test/index";
+        }
+        try {
+            String destFilePath = "E:\\华清远见\\七月\\SpringBoot\\Day8.18（文件上传）\\upload\\"+file.getOriginalFilename();
+            File destFile = new File(destFilePath);
+            file.transferTo(destFile);
+            redirectAttributes.addFlashAttribute("message","Upload file success.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("message","Upload file failed");
+        }
+        return "redirect:/test/index";
+    }
+
+    /**
+     * 127.0.0.1/test/files ------ post
+     */
+    @PostMapping(value = "/files",consumes = "multipart/form-data")
+    public String uploadFiles(@RequestParam MultipartFile[] files,RedirectAttributes redirectAttributes) {
+        boolean empty = true;
+        try {
+            for (MultipartFile file : files) {
+                if (file.isEmpty()) {
+                    continue;
+                }
+                String destFilePath = "E:\\华清远见\\七月\\SpringBoot\\Day8.18（文件上传）\\upload\\" + file.getOriginalFilename();
+                File destFile = new File(destFilePath);
+                file.transferTo(destFile);
+                empty = false;
+            }
+            if (empty){
+                redirectAttributes.addFlashAttribute("message","Please select file.");
+            }else {
+                redirectAttributes.addFlashAttribute("message","Upload file success.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("message","Upload file failed.");
+        }
+        return "redirect:/test/index";
+    }
+
+    /**
+     * 127.0.0.1/test/file ------- get
+     */
+    @GetMapping("/file")
+    public ResponseEntity<Resource> downloadFile(@RequestParam String fileName){
+        Resource resource = null;
+        try {
+            String aa = new String(fileName.getBytes("utf-8"),"ISO-8859-1");
+            resource = new UrlResource(Paths.get("E:\\华清远见\\七月\\SpringBoot\\Day8.18（文件上传）\\upload\\"+fileName).toUri());
+
+            if (resource.exists() && resource.isReadable()){
+                return ResponseEntity.ok().header(
+                        HttpHeaders.CONTENT_TYPE,"application/octet-stream")
+                        .header(HttpHeaders.CONTENT_DISPOSITION,
+                                String.format("attachment;filename=\"%s\"",
+                                        aa)).body(resource);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将文件以BufferedInputStream的方式读取到byte[]里面，然后用OutputStream.write输出文件
+     */
+    @RequestMapping("/download1")
+    public void downloadFile1(HttpServletRequest request,
+                              HttpServletResponse response,
+                              @RequestParam String fileName){
+        String filePath = "E:\\华清远见\\七月\\SpringBoot\\Day8.18（文件上传）\\upload\\"+File.separator+fileName;
+        File downloadFile = new File(filePath);
+        if (downloadFile.exists()){
+            String aa = null;
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            try {
+                aa = new String(fileName.getBytes("utf-8"),"ISO-8859-1");
+                response.setContentType("application/octet-stream");
+                response.setContentLength((int)downloadFile.length());
+                response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                        String.format("attachment;filename=\"%s\"",aa));
+                byte[] buffer = new byte[1024];
+                fis = new FileInputStream(downloadFile);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+                while (i != -1){
+                    os.write(buffer,0,i);
+                    i = bis.read(buffer);
+                }
+            } catch (Exception e) {
+                LOGGER.debug(e.getMessage());
+                e.printStackTrace();
+            }finally {
+                try {
+                    if (fis != null){
+                        fis.close();
+                    }
+                    if (bis != null){
+                        bis.close();
+                    }
+                }catch (Exception e2){
+                    LOGGER.debug(e2.getMessage());
+                    e2.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 以包装类IOUtils输出文件
+     */
+    @RequestMapping("/download2")
+    public void downloadFile2(HttpServletRequest request,
+                              HttpServletResponse response,
+                              @RequestParam String fileName){
+        String filePath = "E:\\华清远见\\七月\\SpringBoot\\Day8.18（文件上传）\\upload\\"+fileName;
+        File downloadFile = new File(filePath);
+        try {
+            if (downloadFile.exists()){
+                String aa = new String(fileName.getBytes("utf-8"),"ISO-8859-1");
+                response.setContentType("application/octet-stream");
+                response.setContentLength((int)downloadFile.length());
+                response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                        String.format("attachment;filename=\"%s\"",aa));
+                InputStream is = new FileInputStream(downloadFile);
+                IOUtils.copy(is,response.getOutputStream());
+                response.flushBuffer();
+            }
+        }catch (Exception e){
+            LOGGER.debug(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
      * 127.0.0.1/test/index  -----  get
      */
     @GetMapping("/index")
@@ -137,6 +295,5 @@ public class TestController {
         modelMap.addAttribute("updateCityUri", "/api/city");
         //modelMap.addAttribute("template", "test/index");
         return "index";
-
     }
 }
